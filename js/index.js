ff94e3c69fc38e8dd8225b3341cd1ad88c96c543
efron15 <fundate0115@gmail.com>
2022-05-10 01:26:49 +0800

    [ feat ]增加註解

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+    // 在畫面開始時執行這段程式
     init()
     
     function init() {
+        // 抓取API網址
         let url = "https://api.covid19api.com/summary"
+        // 設定一個空值後續存放數據用
         let data = ''
 
         $.get(url,function (data) {
